fe4a8b20180c36298ccc604bd32707073dfd2c52
Ololade <73138104+zololade@users.noreply.github.com>
2026-03-26 02:46:26 +0100

    feat: implement a recursive merge sort logic

--- test/fibonacci.test.ts
+++ test/fibonacci.test.ts
@@ -2,7 +2,7 @@ import { fibs, fibsRec } from "../src/fibonacci";
 import { describe, expect, it } from "vitest";
 
 describe("fibonacci sequence", () => {
-  it("expects fibonacci of 8 to be [0, 1, 1, 2, 3, 5, 8, 13] ", () => {
-    expect(fibs(8).sort()).toBe([0, 1, 1, 2, 3, 5, 8, 13].sort());
+  it("expects fibonacci of 7 to be [ 1, 1, 2, 3, 5, 8, 13] ", () => {
+    expect(fibsRec(7).sort()).toEqual([0, 1, 1, 2, 3, 5, 8, 13].sort());
   });
 });
